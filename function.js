1ee5ceadda9f685bc3748dd76383f36e4e786ee0
SiFirmansyahModz <164661353+SiFirmansyahModz@users.noreply.github.com>
2025-04-03 21:39:52 +0700

    Update function.js

--- function.js
+++ function.js
@@ -1,10 +1,9 @@
 <div style="text-align: center;">
-    <img src="telegram.png" alt="Sidiq Firmansyah" style="border-radius: 50%; max-width: 100%; height: auto;"/>
+    <img src="icon.png" alt="Sidiq Firmansyah" style="border-radius: 50%; max-width: 100%; height: auto;"/>
     <h2>Sidiq Firmansyah</h2>
     <p>Available On:</p>
-    <a href="https://t.me/SimplifiedModz" target="_blank" style="text-decoration: none;">
+    <img src="telegram.png href="https://t.me/SimplifiedModz" target="_blank" style="text-decoration: none;">
         <button style="background-color: #0088cc; color: white; border: none; padding: 10px 20px; border-radius: 5px; cursor: pointer;">
-            Telegram
         </button>
-    </a>
+    </img>
 </div>
