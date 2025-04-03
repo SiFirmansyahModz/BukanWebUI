1a4227e0128b5c234996ff24905f0b62933e870d
SiFirmansyahModz <164661353+SiFirmansyahModz@users.noreply.github.com>
2025-04-03 22:08:46 +0700

    Delete function.js

--- function.js
+++
@@ -1,9 +0,0 @@
-<div style="text-align: center;">
-    <img src="icon.png" alt="Sidiq Firmansyah" style="border-radius: 50%; max-width: 100%; height: auto;"/>
-    <h2>Sidiq Firmansyah</h2>
-    <p>Available On:</p>
-    <img src="telegram.png href="https://t.me/SimplifiedModz" target="_blank" style="text-decoration: none;">
-        <button style="background-color: #0088cc; color: white; border: none; padding: 10px 20px; border-radius: 5px; cursor: pointer;">
-        </button>
-    </img>
-</div>
